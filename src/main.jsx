0c4d36f82065edae249c049ba7896810ce500d1e
kolhekrishna <krishnakolhexyz@gmail.com>
2026-07-15 06:15:48 +0530

    Debug TMDB environment variable

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+console.log("TMDB KEY:", import.meta.env.VITE_TMDB_KEY);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
